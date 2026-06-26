fa82cf183e57c5dfca5a785880fb0d2402000a26
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-06-26 13:23:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ import u from '/umbrella.esm.js';
 const normal = u('.normal');
 const special = u('.special');
 const helpLinks = u('.special-link');
+const safetyLink = u('#emergency');
 
+window.addEventListener("devicemotion", (event) => {
+  console.log(`${event.acceleration.x} m/s2`);
+});
+
+safetyLink.on('click', ()=>{
+    showNormal()
+})
 helpLinks.on('click', (event)=>{
     showSpecial(event)
 });
@@ -11,4 +19,9 @@ helpLinks.on('click', (event)=>{
 function showSpecial(e){
     normal.addClass('hidden');
     special.removeClass('hidden');
+}
+
+function showNormal(e){
+    special.addClass('hidden');
+    normal.removeClass('hidden');
 }
