a5b442f6384ea06f5304173996dbbab3ff28cae2
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-06-27 12:50:04 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -7,11 +7,12 @@ const safetyLink = u('#top-link');
 
 window.addEventListener("devicemotion", (event) => {
     let motion = `${event.acceleration.x} m/s2 on x-axis, ${event.acceleration.y} m/s2 on y-axis, ${event.acceleration.z} m/s on z-axis`
-    document.getElementById('motion').innerText = event.acceleration;
-    // if (event.acceleration.x > 3 || event.acceleration.y > 3 || event.acceleration.z){
-    //     // showNormal();
-    //     document.getElementById('motion').innerText = motion;
-    // }
+    // console.log(event.acceleration);
+    // document.getElementById('motion').innerText = motion;
+    if (event.acceleration.x > 3 || event.acceleration.y > 3 || event.acceleration.z > 3){
+        showNormal();
+        // document.getElementById('motion').innerText = motion;
+    }
 });
 
 safetyLink.on('click', ()=>{
