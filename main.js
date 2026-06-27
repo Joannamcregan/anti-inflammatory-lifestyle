91ec81bd0cf6e1f99ddd4d63ac0b491fc9949d61
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-06-27 12:53:37 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ window.addEventListener("devicemotion", (event) => {
     let motion = `${event.acceleration.x} m/s2 on x-axis, ${event.acceleration.y} m/s2 on y-axis, ${event.acceleration.z} m/s on z-axis`
     // console.log(event.acceleration);
     // document.getElementById('motion').innerText = motion;
-    if (event.acceleration.x > 3 || event.acceleration.y > 3 || event.acceleration.z > 3){
+    if (event.acceleration.x > 15 || event.acceleration.y > 15 || event.acceleration.z > 15){
         showNormal();
         // document.getElementById('motion').innerText = motion;
     }
