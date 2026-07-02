4cbe488f8b60162aabc3833d0aef45655437ebd0
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-07-02 10:18:56 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
 import u from './umbrella.esm.js';
 
-const normal = u('.normal');
+// const normal = u('.normal');
 const special = u('.special');
 const helpLinks = u('.special-link');
 const safetyLink = u('#top-link');
 
 window.addEventListener("devicemotion", (event) => {
     let motion = `${event.acceleration.x} m/s2 on x-axis, ${event.acceleration.y} m/s2 on y-axis, ${event.acceleration.z} m/s on z-axis`
-    // console.log(event.acceleration);
-    // document.getElementById('motion').innerText = motion;
     if (event.acceleration.x > 15 || event.acceleration.y > 15 || event.acceleration.z > 15){
         showNormal();
-        // document.getElementById('motion').innerText = motion;
     }
 });
 
-safetyLink.on('click', ()=>{
-    showNormal();
-})
 helpLinks.on('click', (event)=>{
     showSpecial(event);
+    document.getElementById('top-link').addEventListener('click', ()=>{
+        showNormal();
+    });
     if (typeof DeviceMotionEvent.requestPermission === 'function') {
         DeviceMotionEvent.requestPermission();
     }    
@@ -27,11 +24,14 @@ helpLinks.on('click', (event)=>{
 );
 
 function showSpecial(e){
-    normal.addClass('hidden');
+    console.log('showing special');
     special.removeClass('hidden');
+    safetyLink.attr('href', '#mood');
 }
 
-function showNormal(e){
+function showNormal(){
+    console.log('showing normal');
+    safetyLink.attr('href', '#mood');
+    console.log('showing normal')
     special.addClass('hidden');
-    normal.removeClass('hidden');
 }
