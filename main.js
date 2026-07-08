4a1b77d504805adfdca5eb6ae59985c627b18d3e
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-07-08 18:33:02 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,70 +1,56 @@
-import u from './umbrella.esm.js';
+import u from "./umbrella.esm.js";
 
 // const normal = u('.normal');
-const special = u('.special');
-const helpLinks = u('.special-link');
-const safetyLink = u('#top-link');
-const gotIt = u('#got-it');
-const specialNotes = u('.special-note');
-const mobileLink = u('#mobile-link');
-const mobileNav = u('#mobile-nav');
-const mobileClose = u('#mobile-close');
-const links = u('.mobile-link');
+const special = u(".special");
+const normal = u(".normal");
+const safetyLink = u("#top-link");
+const specialNotes = u(".special-note");
+const permissionLink = u("#permission-prompt");
 
-window.addEventListener("devicemotion", (event) => {
-    let motion = `${event.acceleration.x} m/s2 on x-axis, ${event.acceleration.y} m/s2 on y-axis, ${event.acceleration.z} m/s on z-axis`
-    if (event.acceleration.x > 15 || event.acceleration.y > 15 || event.acceleration.z > 15){
-        showNormal();
-    }
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    showNormal();
+  }
 });
 
-links.on('click', closeMobileMenu);
-
-gotIt.on('click', ()=>{
-    specialNotes.addClass('hidden');
-})
-
-helpLinks.on('click', (event)=>{
-    showSpecial(event);
-    document.getElementById('top-link').addEventListener('click', ()=>{
-        showNormal();
-    });
-    if (typeof DeviceMotionEvent.requestPermission === 'function') {
-        DeviceMotionEvent.requestPermission();
-    }    
+window.addEventListener("devicemotion", event => {
+  let motion = `${event.acceleration.x} m/s2 on x-axis, ${event.acceleration.y} m/s2 on y-axis, ${event.acceleration.z} m/s on z-axis`;
+  if (
+    event.acceleration.x > 15 ||
+    event.acceleration.y > 15 ||
+    event.acceleration.z > 15
+  ) {
+    showNormal();
+  }
 });
 
-mobileLink.on('click', (event)=>{
-    mobileNav.addClass('fadeIn');
-    u(event.target).addClass('hidden');
-    setTimeout(()=>{
-        mobileClose.removeClass('hidden');
-    }, 1000)
-})
+permissionLink.on("click", event => {
+  if (typeof DeviceMotionEvent.requestPermission === "function") {
+    DeviceMotionEvent.requestPermission();
+  }
+});
 
-mobileClose.on('click', closeMobileMenu);
+safetyLink.on("click", () => {
+  showNormal();
+});
 
-function showSpecial(e){
-    console.log('showing special');
-    special.removeClass('hidden');
-    safetyLink.attr('href', '#mood');
+function showNormal() {
+  special.addClass("hidden");
+  normal.removeClass("hidden");
 }
 
-function showNormal(){
-    console.log('showing normal');
-    helpLinks.addClass('hidden');
-    safetyLink.attr('href', '#top');
-    console.log('showing normal')
-    special.addClass('hidden');
-}
+// helpLinks.on("click", event => {
+//   showSpecial(event);
+//   document.getElementById("top-link").addEventListener("click", () => {
+//     showNormal();
+//   });
+//   if (typeof DeviceMotionEvent.requestPermission === "function") {
+//     DeviceMotionEvent.requestPermission();
+//   }
+// });
 
-function closeMobileMenu(){
-    mobileNav.addClass('fadeOut');
-    mobileNav.removeClass('fadeIn');
-    mobileClose.addClass('hidden');
-    setTimeout(()=>{
-        mobileLink.removeClass('hidden');
-        mobileNav.removeClass('fadeOut');
-        mobileNav.addClass('hidden');
-    }, 1000)
-}
+// function showSpecial(e) {
+//   console.log("showing special");
+//   special.removeClass("hidden");
+//   safetyLink.attr("href", "#mood");
+// }
